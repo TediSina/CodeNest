3f6fe7df5b6b4f5711b0a1ec3ca1b52fed1f2663
Tedi Sina <81098784+TediSina@users.noreply.github.com>
2024-12-15 17:46:27 +0100

    Add login page.

--- CodeNest/urls.py
+++ CodeNest/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('', include('forum.urls')),
 ]
